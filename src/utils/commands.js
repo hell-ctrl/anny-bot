775038e123d429e5c18721119426f162bde8ae75
m._neto <89698038+hell-ctrl@users.noreply.github.com>
2023-10-29 14:47:18 -0300

    Update commands.js

--- src/utils/commands.js
+++ src/utils/commands.js
@@ -2,8 +2,8 @@ const clc = require("cli-color");
 const dateFormat = require("dateformat");
 
 function printLogs(isGroup, sender, pushName, groupName, command) {
-  console.log(`\n┌══════ « ${clc.bold(`Comando no ${isGroup ? "Grupo" : "Priv>
-  console.log(`│ ⇝ ${isGroup ? `Grupo: ${clc.bold(groupName)}` : `Número: ${s>
+  console.log(`\n┌══════ « ${clc.bold(`Comando no ${isGroup ? "Grupo" : "Privado"}`)} » ══════➣`);
+  console.log(`│ ⇝ ${isGroup ? `Grupo: ${clc.bold(groupName)}` : `Número: ${sender.split("@")[0]}`}`);
   console.log(`│ ⇝ Usuário: ${clc.bold(pushName)}`);
   console.log(`│ ⇝ Comando: ${clc.bold(command)}`);
   console.log(`│ ⇝ Data: ${clc.bold(dateFormat())}`);
